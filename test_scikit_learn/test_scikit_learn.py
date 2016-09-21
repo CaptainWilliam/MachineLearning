02bcdcb98230aa30ced003a7ae0e4645b63c6294
William <lhliu1994@gmail.com>
2016-09-21 09:18:00 +0800

    add some note and split func

--- test_scikit_learn/test_scikit_learn.py
+++ test_scikit_learn/test_scikit_learn.py
@@ -1,6 +1,7 @@
 from scipy.stats import uniform as sp_rand
 from sklearn import preprocessing
 from sklearn import metrics
+from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import RFE
 from sklearn.linear_model import LogisticRegression, Ridge
@@ -14,8 +15,15 @@ data_url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indian
 raw_data = urllib.urlopen(data_url)
 dataset = np.loadtxt(raw_data, delimiter=',')
 input_dataset = dataset[:, :-1]
+print len(input_dataset[0])
+input_dataset_2 = dataset[:, :8]
+print len(input_dataset_2[0])
 output_classification = dataset[:, -1]
 # print input_dataset
+
+# cross_validation(split)
+train_input_dataset, test_input_dataset, train_output_classification, test_output_classification = train_test_split(
+    input_dataset, output_classification, train_size=0.7, random_state=42)
 
 # preprocess
 input_dataset_normalized = preprocessing.normalize(input_dataset)
@@ -42,11 +50,11 @@ and use test dataset to make prediction
 then compare its results with expected y
 '''
 model_3 = GaussianNB()
-model_3.fit(input_dataset, output_classification)
-excepted = output_classification
-predicted = model_3.predict(input_dataset)
-# print metrics.classification_report(excepted, predicted)
-# print metrics.confusion_matrix(excepted, predicted)
+model_3.fit(train_input_dataset, train_output_classification)
+excepted = test_output_classification
+predicted = model_3.predict(test_input_dataset)
+print metrics.classification_report(excepted, predicted)
+print metrics.confusion_matrix(excepted, predicted)
 
 # optimization 1
 '''
@@ -56,12 +64,12 @@ alphas = np.array([1, 0.1, 0.01, 0.001, 0.0001, 0])
 model_4 = Ridge()
 grid = GridSearchCV(estimator=model_4, param_grid=dict(alpha=alphas))
 grid.fit(input_dataset, output_classification)
-print grid.best_score_
-print grid.best_estimator_.alpha
+# print grid.best_score_
+# print grid.best_estimator_.alpha
 
 # optimization 2
 param_grid = {'alpha': sp_rand()}
 rsearch = RandomizedSearchCV(estimator=model_4, param_distributions=param_grid, n_iter=100)
 rsearch.fit(input_dataset, output_classification)
-print rsearch.best_score_
-print rsearch.best_estimator_.alpha
+# print rsearch.best_score_
+# print rsearch.best_estimator_.alpha
