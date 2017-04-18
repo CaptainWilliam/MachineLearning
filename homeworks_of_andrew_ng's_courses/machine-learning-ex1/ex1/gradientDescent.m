08b2997b3c942614bc5f49e1a7c257d35c4ca12f
William <lhliu1994@gmail.com>
2017-04-18 16:38:42 +0800

    altered typesetting

--- homeworks_of_andrew_ng's_courses/machine-learning-ex1/ex1/gradientDescent.m
+++ homeworks_of_andrew_ng's_courses/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,11 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     
-    % theta -=  (alpha / m) * sum((X * theta - y) .* X))'
-    theta -=  (alpha / m) * (X' * (X * theta - y));
-
-
-
+    % theta -=  (alpha / m) * sum((X * theta - y) .* X)'
+    theta -=  (alpha / m) * (X' * (X * theta - y);
 
 
 
